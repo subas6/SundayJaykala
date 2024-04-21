695dea3dc47bcb02ef0c892a0f5d673c6dd6a7a6
subas6 <115884598+subas6@users.noreply.github.com>
2024-04-21 11:49:15 +0530

    Update LoginPage.java

--- inetBankingV1/src/test/java/com/inetbanking/pageObjects/LoginPage.java
+++ inetBankingV1/src/test/java/com/inetbanking/pageObjects/LoginPage.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
+	//hiii guys wel come 
 	WebDriver ldriver;
 	LoginPage(WebDriver rdriver)
 	{
